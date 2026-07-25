ec0f3661915be983f503c7f13bf5b87cc8dc776e
vico1022 <vico1022@gmail.com>
2026-07-25 18:12:18 +0800

    Update function.sh

--- Scripts/function.sh
+++ Scripts/function.sh
@@ -123,7 +123,6 @@ function set_kernel_size() {
   sed -i "/^define Device\/linksys_mr/,/^endef/ { /KERNEL_SIZE := 8192k/s//KERNEL_SIZE := 12288k/ }" $image_file
   sed -i "/^define Device\/link_nn6000-v2/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" $image_file
 }
-}
 #开启内存回收补丁
 function enable_skb_recycler() {
   cat >> $1 <<EOF
